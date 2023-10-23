6c22eb5ea57a19494322fa57e0a041d2a42bdde2
Dai <let__it__be@hb.tp1.jp>
2023-10-23 11:44:03 +0900

    231022footer03追加

---
+++ components/Layout/footer/footer03.php
@@ -0,0 +1,61 @@
+<?php 
+  $uri = get_theme_file_uri();
+?>
+<footer class="footer03">
+    <div class="innerFooter">
+        <div class="footerTop">
+            <div class="left">
+                <a href="<?=$site_url;?>" class="logo">
+                    <img src="<?=$uri?>/img/common/logo.svg" alt="">
+                </a>
+            </div>
+            <div class="right">
+                <div class="row">
+                    <ul>
+                        <li><a href="" class="title">トップページ<span>Top</span></a></li>
+                    </ul>
+                    <ul>
+                        <li><a href="" class="title">会社情報<span>Company</span></a></li>
+                        <li><a href="" class="title">- 社長メッセージ</a></li>
+                        <li><a href="" class="title">- 企業理念</a></li>
+                        <li><a href="" class="title">- 概要</a></li>
+                        <li><a href="" class="title">- 沿革</a></li>
+                        <li><a href="" class="title">- グループ会社</a></li>
+                    </ul>
+                </div>
+                <div class="row">
+                    <ul>
+                        <li><a href="" class="title">製品情報<span>Business</span></a></li>
+                        <li><a href="" class="title">- 事業説明</a></li>
+                        <li><a href="" class="title">- 業績</a></li>
+                        <li><a href="" class="title">- 製品技術紹介</a></li>
+                        <li><a href="" class="title">- 製品ができるまで</a></li>
+                    </ul>
+                    <ul>
+                        <li><a href="" class="title">採用情報<span>Recruit</span></a></li>
+                        <li><a href="" class="title">- 新卒採用</a></li>
+                        <li><a href="" class="title">- 中途採用</a></li>
+                    </ul>
+                </div>
+                <div class="row lastRow">
+                    <ul>
+                        <li><a href="" class="title">自社ブログ<span>Blog</span></a></li>
+                    </ul>
+                    <ul>
+                        <li><a href="" class="title">お問い合わせ<span>Contact</span></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        
+        <div class="footerBottom">
+            <ul class="subNavi">
+                <li><a href="">個人情報保護方針<img src="<?=$uri?>/img/footer/footer03/page.svg" alt=""></a></li>
+                <li><a href="">個人情報の取り扱いについて<img src="<?=$uri?>/img/footer/footer03/page.svg" alt=""></a></li>
+            </ul>
+            <div class="ci">
+                <p class="copyright"><span>©</span><?=date('Y') ?> copyright</p>
+            </div>
+        </div> 
+    </div>
+</footer>

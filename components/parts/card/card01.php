2e29db04a77a3aaee1078959060dea83aa54d95a
Dai <let__it__be@hb.tp1.jp>
2023-11-04 13:55:59 +0900

    231104card01追加

---
+++ components/parts/card/card01.php
@@ -0,0 +1,46 @@
+<div class="card01">
+    <div class="cardItem">
+        <div class="image">
+            <img src="https://picsum.photos/id/237/800/600" alt="" />
+        </div>
+        <div class="title">
+            <span class="tag">ジャンル</span>
+            <h3>商品名が入ります。商品名が入ります。商品名が入ります。商品名が入ります。</h3>
+            <p class="miniText">2023年11月掲載</p>
+        </div>
+        <p class="lead">本文が入ります。本文が入ります。本文が入ります。本文が入ります。本文が入ります。本文が入ります。本文が入ります。本文が入ります。本文が入ります。本文が入ります。</p>
+        <a href="" class="btn">
+            記事を読む<span><svg xmlns="http://www.w3.org/2000/svg" width="7.823" height="14.231" viewBox="0 0 7.823 14.231"><path id="パス_1370" data-name="パス 1370" d="M-1489.847-8987.893l6.409,6.408-6.409,6.409" transform="translate(1490.554 8988.6)" fill="none" stroke="#367417" stroke-linecap="round" stroke-width="1"/></svg></span>
+        </a>
+    </div>
+
+    <div class="cardItem">
+        <div class="image">
+            <img src="https://picsum.photos/id/238/800/600" alt="" />
+        </div>
+        <div class="title">
+            <span class="tag">ジャンル</span>
+            <h3>商品名が入ります。商品名が入ります。</h3>
+            <p class="miniText">2023年11月掲載</p>
+        </div>
+        <p class="lead">本文が入ります。本文が入ります。本文が入ります。本文が入ります。本文が入ります。本文が入ります。本文が入ります。本文が入ります。本文が入ります。本文が入ります。本文が入ります。本文が入ります。</p>
+        <a href="" class="btn">
+            記事を読む<span><svg xmlns="http://www.w3.org/2000/svg" width="7.823" height="14.231" viewBox="0 0 7.823 14.231"><path id="パス_1370" data-name="パス 1370" d="M-1489.847-8987.893l6.409,6.408-6.409,6.409" transform="translate(1490.554 8988.6)" fill="none" stroke="#367417" stroke-linecap="round" stroke-width="1"/></svg></span>
+        </a>
+    </div>
+
+    <div class="cardItem">
+        <div class="image">
+            <img src="https://picsum.photos/id/239/800/600" alt="" />
+        </div>
+        <div class="title">
+            <span class="tag">ジャンル</span>
+            <h3>商品名が入ります。商品名が入ります。</h3>
+            <p class="miniText">2023年11月掲載</p>
+        </div>
+        <p class="lead">本文が入ります。本文が入ります。本文が入ります。本文が入ります。本文が入ります。</p>
+        <a href="" class="btn">
+            記事を読む<span><svg xmlns="http://www.w3.org/2000/svg" width="7.823" height="14.231" viewBox="0 0 7.823 14.231"><path id="パス_1370" data-name="パス 1370" d="M-1489.847-8987.893l6.409,6.408-6.409,6.409" transform="translate(1490.554 8988.6)" fill="none" stroke="#367417" stroke-linecap="round" stroke-width="1"/></svg></span>
+        </a>
+    </div>
+</div>
--- index.php
+++ index.php
@@ -7,6 +7,7 @@
 
 <main class="top">
     <?php get_template_part('components/parts/mega/mega01'); ?>
+    <?php get_template_part('components/parts/card/card01'); ?>
     <!-- <div class="box boxA">トップページが入ります。</div>
     <div class="box boxB"><h2>トップページが入ります。</h2></div>
     <div class="conditionBoxA">トップページが入ります。</div>

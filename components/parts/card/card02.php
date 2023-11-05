9a342fa3291af3c66690d23627f688a936d10c94
Dai <let__it__be@hb.tp1.jp>
2023-11-05 14:01:25 +0900

    231105card02追加

---
+++ components/parts/card/card02.php
@@ -0,0 +1,49 @@
+<ul class="card02">
+    <li class="cardItem">
+        <div class="cardImage">
+            <a href="#link1"><img src="https://picsum.photos/id/237/800/600" alt="" /></a>
+        </div>
+        <h3>リンクの中にあるリンク・ボタンの中にあるボタンを実装</h3>
+        <p class="cardDescription">
+        <H></H>TMLの仕様上素直に実装できない、マークアップがしづらいUIがたまにあります。このセッションでは、そうした問題を回避しながら、特殊な仕様を実現するための注意点や工夫について紹介します。
+        </p>
+        <div class="cardAuthor">
+            <a class="cardAuthorLink" href="#tag1">
+                <span class="cardAvatar"></span>link within link
+            </a>
+        </div>
+        <a href="#link1" class="link"></a>
+    </li>
+
+    <li class="cardItem">
+        <div class="cardImage">
+            <a href="#link2"><img src="https://picsum.photos/id/238/800/600" alt="" /></a>
+        </div>
+        <h3>タイトルが入ります。</h3>
+        <p class="cardDescription">
+            HTMLの仕様上素直に実装できない、マークアップがしづらいUIがたまにあります。このセッションでは、そうした問題を回避しながら、特殊な仕様を実現するための注意点や工夫について紹介します。
+        </p>
+        <div class="cardAuthor">
+            <a class="cardAuthorLink" href="#tag2">
+                <span class="cardAvatar"></span>link within link
+            </a>
+        </div>
+        <a href="#link2" class="link"></a>
+    </li>
+
+    <li class="cardItem">
+        <div class="cardImage">
+            <a href="#link3"><img src="https://picsum.photos/id/239/800/600" alt="" /></a>
+        </div>
+        <h3>タイトルが入ります。タイトルが入ります。タイトルが入ります。タイトルが入ります。タイトルが入ります。</h3>
+        <p class="cardDescription">
+            HTMLの仕様上素直に実装できない、マークアップがしづらいUIがたまにあります。
+        </p>
+        <div class="cardAuthor">
+            <a class="cardAuthorLink" href="#tag3">
+                <span class="cardAvatar"></span>link within link
+            </a>
+        </div>
+        <a href="#link3" class="link"></a>
+    </li>
+</ul>

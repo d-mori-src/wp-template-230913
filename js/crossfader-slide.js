f6ce14070a69d8f7c191af763602caa601b8fc02
Dai <let__it__be@hb.tp1.jp>
2025-03-19 16:12:42 +0900

    250319crossfader-slider スムーズに切り替えられるように改修

--- js/crossfader-slide.js
+++ js/crossfader-slide.js
@@ -1,9 +1,9 @@
 // スライドショーの設定
 const sliderConfig = {
     titles: [
-        '<span>This is sub catch</span>メインキャッチ<br />入ります',
-        '<span>This is sub catch</span>メインキャッチ<br />入ります',
-        '<span>This is sub catch</span>メインキャッチ<br />入ります',
+        '<span>JSub Catch</span>メイン<br />タイトル1',
+        '<span>JSub Catch</span>メイン<br />タイトル2',
+        '<span>JSub Catch</span>メイン<br />タイトル3',
     ],
     duration: 4000,
     autoplay: true,
@@ -28,24 +28,50 @@ sliderConfig.titles.forEach(() => {
 });
 
 /**
+ * 直前に表示されていた画像のインデックスを計算する
+ * @param {number} currentIndex - 現在のインデックス
+ * @param {number} arrayLength - 配列の長さ
+ * @returns {number} 直前のインデックス
+ */
+const getPreviousIndex = (currentIndex, arrayLength) => {
+    return (currentIndex - 1 + arrayLength) % arrayLength;
+};
+
+/**
  * 画像の切り替え処理
  * @param {number} next - 次に表示する画像のインデックス
  */
 const switchImage = (next) => {
     const current = sliderConfig.defaultIndex; // 現在のインデックスを保持
+    const previous = getPreviousIndex(current, sliderConfig.elInner.length); // 直前のインデックスを計算
 
-    sliderConfig.elInner[current].classList.remove('is-visible');
-    setTimeout(() => {
+    // 直前の画像からis-visibleクラスを削除
+    sliderConfig.elInner[previous].classList.remove('is-visible');
+
+    // z-indexとopacityをリセット
+    sliderConfig.elInner.forEach((inner) => {
+        Object.assign(inner.style, {
+            zIndex: '0',
+            opacity: '0'
+        });
+    });
+
+    // 次の画像を表示する
+    requestAnimationFrame(() => {
         sliderConfig.elInner[next].classList.add('is-visible');
         updateTitle(next);
         sliderConfig.defaultIndex = next; // アニメーション完了後にインデックスを更新
+        Object.assign(sliderConfig.elInner[next].style, {
+            zIndex: '1',
+            opacity: '1'
+        });
         if (sliderConfig.autoplay) {
             startAutoplay();
         }
         if (sliderConfig.dots) {
             updateDots(next); // dotsの状態を更新
         }
-    }, 100); // アニメーション時間に合わせて調整
+    });
 };
 
 /**
